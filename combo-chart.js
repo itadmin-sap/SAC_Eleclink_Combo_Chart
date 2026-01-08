63ccdbf0f93b463cdcef8f2302365225278c6a88
basimwangde <basim.wangde@percipere.co>
2026-01-08 17:53:24 +0530

    overlapping

--- combo-chart.js
+++ combo-chart.js
@@ -566,7 +566,9 @@
               }
             },
             datalabels: {
-              display: true
+              display: "auto",   // allow auto-skip when overlapping
+              clamp: true,       // keep labels within chart area
+              clip: true         // clip labels that still go out
             }
           },
           scales: {
